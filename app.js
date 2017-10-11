ca493f5b853a4d014261e7897be85932b4bd3fef
Yokipi <mailmuled@gmail.com>
2017-10-10 19:42:09 -0500

    Blank canvas

---
+++ app.js
@@ -0,0 +1,18 @@
+var canvas;
+var gl;
+
+function init() {
+  canvas = document.querySelector("#glCanvas");
+  gl = canvas.getContext("webgl");
+
+  // Only continue if WebGL is available and working
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
+
+  gl.clearColor(0.0, 0.0, 40.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+init();
